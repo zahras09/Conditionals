c226ce89b109dd5cb74bcd12633ad23458808cbd
zahras09 <michaelpark@Michaels-MacBook-Pro.local>
2016-04-24 14:13:29 -0700

    completed conditionals exercises

---
+++ conditionals.py
@@ -0,0 +1,114 @@
+#1. pair's name conditionals:
+# name = raw_input("what is your name?")
+# expected_name = "ali"
+# # my_name = "zahra"
+# # partner_name = "ali"
+# partner_name = raw_input("What is your partner's name?")
+# # if my_name > partner_name:
+# if expected_name == name:
+# 	print "My name is greater"
+# elif my_name < partner_name:
+# 	print "Their name is greater"
+# else:
+# 	print "Our names are equal"
+
+# # #2. Date Conditionals:
+# date = int(raw_input("what is today's date?"))
+# if date <= 31 and date >= 15:
+# 	# print "invalid date"
+# # elif date >= 15:
+# 	print "Oh, we're halfway there!"
+# elif date < 15:
+# 	print "The month is still young"
+# else:
+# 	print "invalid date"
+	
+
+# # #3. Day of the week Conditionals:
+# today = raw_input("What day of the week is it?")
+# today = today.lower()
+# if today == "monday":
+# 	print "Yay class day!"
+# elif today == "Tuesday" or "tuesday":
+# 	print  "Sigh, it's only Tuesday"
+# elif today == "Wednesday":
+# 	print "Humpday"
+# elif today == "Thursday":
+# 	print "#tbt"
+# elif today == "Friday":
+# 	print "TGIF!"
+# else:
+# 	print "Yeah, it's the weekend!"
+
+# #The remaining Conditionals exercise:
+#1,2
+# age = int(raw_input("What is your age?"))
+# if age >= 18:
+# 	print "Yay! I can vote!"
+# else:
+# 	print "Awww, I cannot vote"
+# #3
+# age = int(raw_input("what is your age?"))
+# if age >= 18 and age >= 21:
+# 	print "I can vote and go to a bar!"
+
+# #4
+# # age = int(raw_input("What is your age?"))
+# if age >= 21:
+# 	print "I can go to a bar"
+# elif 21 > age >= 18:
+# 	print "I can vote but I cannot go to a bar"
+# else:
+# 	print "I cannot vote or go to a bar"
+
+# # #5
+# number = 8
+# if number % 2 == 0:
+# 	print "The number 8 is even" 
+# else:
+# 	print "The number 8 is odd"
+#6
+# number = 8
+# if number % 2 != 0:
+# 	print "The number 8 is odd"
+# else:
+# 	print "The number 8 is even"
+
+#7
+# number_one = 8
+# number_two = 9 
+# if number_one % 2 == 0 and number_two % 2 == 0:
+# 	print "Both numbers are even."
+# else:
+# 	print "Both numbers are not even."
+
+# #8
+# number_one = 8
+# number_two = 9 
+# if number_one % 2 == 0 and number_two % 2 == 0:
+# 	print "Both numbers are even."
+# elif number_one % 2 ==0 and number_two % 2 !=0:
+# 	print "8 is even and 9 is odd"
+# else:
+# 	print "Both numbers are odd"
+
+
+# #9
+# favorite_color = raw_input("What is your favorite color?")
+# favorite_color = favorite_color.lower()
+# if favorite_color == "blue":
+
+# 	print "My favorite color is blue!"
+# else:
+# 	print "My favorite color is not blue."
+# 10
+favorite_color = raw_input("What is your favorite color?")
+favorite_color = favorite_color.lower()
+if favorite_color == "blue" or favorite_color =="red" or favorite_color == "yellow":
+	print "My favorite color is primary color."
+else:
+	print "My favorite color is a secondary color."
+
+
+
+
